19c9a56d42eded000032f61df73d18b479d4ca7d
Inakshi <inakshi.8734@gmail.com>
2026-09-10 22:24:40 +0530

    Implement function to add digits until single digit.

---
+++ add_digits.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int addDigits(int num) {
+    while (num >= 10) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        num = sum;
+    }
+    return num;
+}
+
+int main() {
+    int num;
+
+    cout << "enter a number: ";
+    cin >> num;
+
+    cout << "single digit sum: " << addDigits(num) << endl;
+
+    return 0;
+}
